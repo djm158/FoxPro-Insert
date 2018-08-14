4e0dd4ca6a042226a20634b0eb0c185805f7326f
Dan McGrath <daniel.mcgrath9@gmail.com>
2018-08-14 17:35:39 -0400

    initial commit

---
+++ Foxpro-Insert-Repro/Inserter/Program.cs
@@ -0,0 +1,71 @@
+﻿using InserterLibrary;
+using System;
+using System.Data;
+using System.IO;
+
+namespace Inserter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var foxProPrefix = "Provider=VFPOLEDB.1;Data Source=";
+            // change this
+            var dbPath = @"C:\Users\dmcgrath\source\repos\FoxPro-Insert\Foxpro-Insert-Repro\Database\main.dbc";
+            Console.WriteLine(dbPath);
+
+            var categoriesGood = "categoryauto";
+            var categoriesBad = "categorynewid";
+
+            var conString = $"{foxProPrefix}{dbPath}";
+
+            Console.WriteLine(conString);
+
+            FoxproWrapper wrapper = new FoxproWrapper(conString);
+
+            // insert a record into auto incrementing table
+            wrapper.InsertRecord(categoriesGood, GetRandomString());
+
+            // get all the categories
+            var categories = wrapper.GetAllData(categoriesGood);
+            PrintTable(categories);
+
+            wrapper.InsertCustomer("dmcgrath");
+            var customers = wrapper.GetAllData("customers");
+            PrintTable(customers);
+            //wrapper.InsertRecord(categoriesBad, GetRandomString());
+
+            // pause execution
+            Console.ReadLine();
+        }
+
+        // use Path.GetRandomFileName() to generate a random string
+        public static string GetRandomString()
+        {
+            string path = Path.GetRandomFileName();
+            path = path.Replace(".", ""); // Remove period.
+            return path;
+        }
+
+        public static void PrintTable(DataTable table)
+        {
+            // write out column names
+            foreach (DataColumn col in table.Columns)
+            {
+                Console.Write("{0,-14}", col.ColumnName);
+            }
+
+            Console.WriteLine();
+
+            // write out data
+            foreach (DataRow row in table.Rows)
+            {
+                foreach (DataColumn col in table.Columns)
+                {
+                    Console.Write("{0,-14}", row[col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Foxpro-Insert-Repro/InsterterLibrary/FoxproWrapper.cs
@@ -0,0 +1,65 @@
+﻿using System.Data;
+using System.Data.OleDb;
+
+namespace InserterLibrary
+{
+    public class FoxproWrapper
+    {
+        private string connectionString;
+
+        public FoxproWrapper(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public DataTable GetAllData(string table)
+        {
+            var queryText = $"select * from {table}";
+            var data = new DataTable();
+
+            using (OleDbConnection connectionHandler = new OleDbConnection(connectionString))
+            {
+                var da = new OleDbDataAdapter();
+                var query = new OleDbCommand(queryText, connectionHandler);
+                connectionHandler.Open();
+                da.SelectCommand = query;
+
+                da.Fill(data);
+                connectionHandler.Close();
+
+            }
+
+            return data;
+        }
+
+        public void InsertRecord(string table, string category)
+        {
+            using (OleDbConnection connectionHandler = new OleDbConnection(connectionString))
+            {
+                var insertText = $"insert into {table} (catname) values (?)";
+                var insertCommand = new OleDbCommand(insertText, connectionHandler);
+
+                insertCommand.Parameters.Add("category", OleDbType.Char).Value = category;
+
+                connectionHandler.Open();
+                insertCommand.ExecuteNonQuery();
+                connectionHandler.Close();
+            }
+        }
+
+        public void InsertCustomer(string name)
+        {
+            using (OleDbConnection connectionHandler = new OleDbConnection(connectionString))
+            {
+                var insertText = $"insert into customers (firstname) values (?)";
+                var insertCommand = new OleDbCommand(insertText, connectionHandler);
+
+                insertCommand.Parameters.Add("firstname", OleDbType.Char).Value = name;
+
+                connectionHandler.Open();
+                insertCommand.ExecuteNonQuery();
+                connectionHandler.Close();
+            }
+        }
+    }
+}
